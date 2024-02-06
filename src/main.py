2d51132c9d0ee92c738d6a0084340eff780324d9
Florin Salasan <florinsalasan@gmail.com>
2024-02-06 09:55:54 -0500

    running into some errors, this will be a backburner project for some time most likely

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
-from ocr_module import convert_pdf_to_images, perform_ocr_on_images
+from ocr_module import convert_pdf_to_images, ocr_on_all, custom_preprocess 
 
 def main(pdf_path):
     # Step 1: Convert PDF to images
     output_folder = 'output_images/'
     image_paths = convert_pdf_to_images(pdf_path, output_folder)
 
-    # Step 2: Perform OCR on images
-    extracted_texts = perform_ocr_on_images(image_paths)
+    # Step 2: Perform preprocessing on images
+    preprocessed_images = custom_preprocess(image_paths)
 
-    # Step 3: Print or process the extracted texts
+    # Step 3: Perform OCR
+    extracted_texts = ocr_on_all(preprocessed_images)
+
+    # Step 4: Print or process the extracted texts
     for i, text in enumerate(extracted_texts):
         print(f"Text from page {i + 1}:\n{text}\n")
+
 
 if __name__ == "__main__":
     # Provide the path to the PDF file as a command-line argument
