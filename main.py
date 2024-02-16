4a3e65ada14d83c49448108515dc15303cfa098a
Florin Salasan <florinsalasan@gmail.com>
2024-02-16 15:43:52 -0500

    got to the point of drawing on the canvas, not anywhere near what I want, also only works on main display currently

--- main.py
+++ main.py
@@ -6,69 +6,29 @@ import subprocess
 import time
 
 
-global overlay
-global count
-count = 0
+def get_x_and_y(event):
+    global lasx, lasy
+    lasx, lasy = event.x, event.y
 
 
-def create_overlay():
-    global is_overlay_active
-    global overlay
+def draw_smth(event):
+    global lasx, lasy
+    canvas.create_rectangle((lasx, lasy, event.x, event.y),
+                            fill='red',
+                            width=1)
+    lasx, lasy = event.x, event.y
 
-    # Create root window and set transparent background
-    overlay = Tk()
-    screen_width = overlay.winfo_screenwidth()
-    screen_height = overlay.winfo_screenheight()
-    overlay.geometry('{0}x{1}'.format(str(screen_width), str(screen_height)))
-    print(screen_width, screen_height)
-    overlay.attributes("-alpha", 0.5)
-    overlay.wm_attributes('-topmost', 1)
-    overlay.bg = Canvas(overlay, width=screen_width, height=screen_height, bg='white')
-    overlay.bg.pack()
-    overlay.mainloop()
+# Create root window and set transparent background
+overlay = Tk()
+screen_width = overlay.winfo_screenwidth()
+screen_height = overlay.winfo_screenheight()
+overlay.geometry('{0}x{1}'.format(str(screen_width), str(screen_height)))
+print(screen_width, screen_height)
+overlay.attributes("-alpha", 0.5)
+canvas = Canvas(overlay, width=screen_width, height=screen_height, bg='white')
+canvas.pack(anchor="nw", fill="both", expand=1)
 
-    print("overlay active")
-    is_overlay_active = True
-    close_overlay_after_timeout(overlay)
-    listener = mouse.Listener(
-        on_move=on_move,
-        on_click=on_click)
-    listener.start()
+canvas.bind("<Button-1>", get_x_and_y)
+canvas.bind("<B1-Motion>", draw_smth)
 
-
-def close_overlay(overlay):
-    overlay.destroy()
-
-
-def close_overlay_after_timeout(overlay):
-    print("timer started")
-    global is_overlay_active
-
-    # Wait for 5 seconds
-    for _ in range(5):
-        if not is_overlay_active:
-            return  # Timer cancelled, exit thread
-        time.sleep(1)
-
-    # Timeout reached, close the overlay
-    close_overlay(overlay)
-    print('about to stop inside timeout function')
-    pynput.mouse.Listener.stop
-
-
-def on_move(x, y):
-    print("Pointer moved to {0}".format((x, y)))
-
-
-def on_click(x, y, button, pressed):
-    global pressed_location
-    global released_location
-    print(pressed)
-    if pressed:
-        pressed_location = x, y
-    else:
-        released_location = x, y
-        print('You pressed at {0}x{1} and released at {2}x{3}'.format(*pressed_location, *released_location))
-
-
-create_overlay()
+overlay.mainloop()
