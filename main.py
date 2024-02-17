f254b8da9c7875c467e6000c8ef053c438b11f91
Florin Salasan <florinsalasan@gmail.com>
2024-02-17 15:04:00 -0500

    can now draw rectangles thanks to an exercise and solution from w3resource for tkinter canvas and graphics 3. Now have the coords for the 4 corners of the area I want to capture

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
-import threading
-from tkinter import *
-import pynput
-from pynput import mouse
-import subprocess
-import time
+import tkinter as tk
 
 
-def get_x_and_y(event):
-    global lasx, lasy
-    lasx, lasy = event.x, event.y
+class Selector:
+
+    def __init__(self, root):
+        self.root = root
+
+        self.screen_height = root.winfo_screenheight()
+        self.screen_width = root.winfo_screenwidth()
+        self.canvas = tk.Canvas(root, width=self.screen_width, height=self.screen_height, bg='white')
+        self.canvas.pack()
+
+        self.root.attributes('-alpha', 0.4)
+
+        self.canvas.bind("<ButtonPress-1>", self.start_draw)
+        self.canvas.bind("<ButtonRelease-1>", self.stop_draw)
+
+        self.drawing = False
+        self.start_x, self.start_y = None, None
+
+        self.coords = {
+            'start_x': None,
+            'start_y': None,
+            'stop_x': None,
+            'stop_y': None,
+        }
+        self.rectangles = []
+
+    def start_draw(self, event):
+        self.drawing = True
+        self.start_x, self.start_y = event.x, event.y
+
+    def stop_draw(self, event):
+        if self.drawing:
+            end_x, end_y = event.x, event.y
+            print(end_x, end_y)
+            rectangle = self.canvas.create_rectangle(
+                self.start_x, self.start_y, end_x, end_y, outline="black", width=1,
+            )
+            self.rectangles.append(rectangle)
+            self.drawing = False
+            self.coords['start_x'] = self.start_x
+            self.coords['start_y'] = self.start_y
+            self.coords['stop_x'] = end_x
+            self.coords['stop_y'] = end_y
+            print(self.coords)
 
 
-def draw_smth(event):
-    global lasx, lasy
-    canvas.create_rectangle((lasx, lasy, event.x, event.y),
-                            fill='red',
-                            width=1)
-    lasx, lasy = event.x, event.y
-
-# Create root window and set transparent background
-overlay = Tk()
-screen_width = overlay.winfo_screenwidth()
-screen_height = overlay.winfo_screenheight()
-overlay.geometry('{0}x{1}'.format(str(screen_width), str(screen_height)))
-print(screen_width, screen_height)
-overlay.attributes("-alpha", 0.5)
-canvas = Canvas(overlay, width=screen_width, height=screen_height, bg='white')
-canvas.pack(anchor="nw", fill="both", expand=1)
-
-canvas.bind("<Button-1>", get_x_and_y)
-canvas.bind("<B1-Motion>", draw_smth)
-
-overlay.mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = Selector(root)
+    root.mainloop()
