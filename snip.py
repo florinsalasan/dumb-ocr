17b98d51737a24d161b69bd7d77b85d3a261a460
Florin Salasan <florinsalasan@gmail.com>
2024-02-26 12:50:06 -0500

    removed excessive print statements

--- snip.py
+++ snip.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import time
 from PIL import ImageGrab
 
 
@@ -12,17 +13,13 @@ class Selector:
         self.root.geometry(str(self.screen_width)+'x'+str(self.screen_height)+'+0+0')
         self.canvas = tk.Canvas(root, width=self.screen_width, height=self.screen_height, bg='white')
         self.canvas.pack()
-        print(self.screen_width, self.screen_height)
         # Used image grab to see what the real resolution of my screen was,
         # noticed it was twice what tk was reporting so I scaled the self.coords
         # by 2 and now it is grabbing the matching selected area by the user.
         img = ImageGrab.grab()
         self.size = img.size
-        print('above is screen_width, below is self.size')
-        print(self.size)
         self.width_ratio = self.size[0] / self.screen_width
         self.height_ratio = self.size[1] / self.screen_height
-        print(self.width_ratio, self.height_ratio)
 
         self.root.attributes('-alpha', 0.2)
 
@@ -50,7 +47,8 @@ class Selector:
         if self.drawing:
             end_x, end_y = event.x, event.y
             self.canvas.create_rectangle(
-                self.start_x, self.start_y, end_x, end_y, outline="black", width=1,
+                self.start_x, self.start_y, end_x, end_y,
+                outline="black", width=1,
             )
             self.drawing = False
             self.coords['start_x'] = self.start_x * self.width_ratio
@@ -65,7 +63,6 @@ if __name__ == '__main__':
     root = tk.Tk()
     app = Selector(root)
     root.mainloop()
-    print(app.rectangles)
 
     # ImageGrab bbox is left, top, right, bottom values
     ig_top = min(app.rectangles[0]['start_y'], app.rectangles[0]['stop_y'])
@@ -75,5 +72,4 @@ if __name__ == '__main__':
 
     # get the image using ImageGrab
     selected_area = ImageGrab.grab(bbox=(ig_left, ig_top, ig_right, ig_bottom))
-    print(ig_top, ig_bottom, ig_left, ig_right)
     selected_area.save('ss.png')
